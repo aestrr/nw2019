f438eafd10cc29385a6c99b5ff0ff6a51754713c
kians376 <kianshah376@gmail.com>
2019-01-27 09:31:35 -0800

    Made Budget Singleton

--- app/src/main/java/com/nwhacks/myapplication/Model/Budget.java
+++ app/src/main/java/com/nwhacks/myapplication/Model/Budget.java
@@ -4,18 +4,23 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Budget {
-    private final double initialMonthlyBudget;
+    private static Budget budget;
+    private double initialMonthlyBudget;
     private double currentBudget;
-    private final Calendar initialBudgetSetTime;
+    private Calendar initialBudgetSetTime;
 
-    public Budget(double monthlyBudget) {
+    public static Budget getInstance() {
+        if (budget == null) {
+            budget = new Budget(0.0);
+        }
+
+        return budget;
+    }
+
+    private Budget(double monthlyBudget) {
         this.initialMonthlyBudget = monthlyBudget;
         this.currentBudget = monthlyBudget;
         this.initialBudgetSetTime = Calendar.getInstance();
-    }
-
-    public void setCurrentBudget(double initialMonthlyBudget) {
-        this.currentBudget = initialMonthlyBudget;
     }
 
     public void spend(double amount) {
@@ -46,4 +51,19 @@ public class Budget {
         return initialMonthlyBudget - currentBudget;
     }
 
+    public static void setBudget(Budget budget) {
+        Budget.budget = budget;
+    }
+
+    public void setInitialMonthlyBudget(double initialMonthlyBudget) {
+        this.initialMonthlyBudget = initialMonthlyBudget;
+    }
+
+    public void setCurrentBudget(double currentBudget) {
+        this.currentBudget = currentBudget;
+    }
+
+    public void setInitialBudgetSetTime(Calendar initialBudgetSetTime) {
+        this.initialBudgetSetTime = initialBudgetSetTime;
+    }
 }
