3126169ba5f59c4e9177043f81d5ba3b55e1ea0a
brandon13331 <brandon13331@gmail.com>
2019-01-26 19:36:07 -0800

    status progress

--- app/src/main/java/com/nwhacks/myapplication/FullscreenActivity.java
+++ app/src/main/java/com/nwhacks/myapplication/FullscreenActivity.java
@@ -1,6 +1,7 @@
 package com.nwhacks.myapplication;
 
 import android.annotation.SuppressLint;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ app/src/main/java/com/nwhacks/myapplication/StatusActivity.java
@@ -0,0 +1,33 @@
+package com.nwhacks.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ProgressBar progressBar = findViewById(R.id.bar_progress);
+        progressBar.setProgress(10);
+    }
+
+}
